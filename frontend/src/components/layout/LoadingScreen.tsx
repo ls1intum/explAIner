31ba3e218c04f4a9e60ac6becb0f54685b4ff39b
martin-stierlen <martin.stierlen@tum.de>
2026-01-28 11:53:56 +0100

    feat(frontend): init components/layout

---
+++ frontend/src/components/layout/LoadingScreen.tsx
@@ -0,0 +1,7 @@
+export default function LoadingScreen() {
+  return (
+    <div>
+      {/* Full-page loader */}
+    </div>
+  );
+}
---
+++ frontend/src/components/layout/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav>
+      {/* Top navigation */}
+    </nav>
+  );
+}
