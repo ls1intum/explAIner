882b3ff27f1f671aca97fd3dbb04b35f307911c5
martin-stierlen <martin.stierlen@tum.de>
2026-02-07 12:31:58 +0100

    refactor(frontend): remove unused class utility function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,10 +1,4 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
+// used for LoadingScreen
 export const getRandomMessage = (messages: string[]): string => {
   return messages[Math.floor(Math.random() * messages.length)];
 };
