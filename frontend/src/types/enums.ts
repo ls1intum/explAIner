7bb2356c41dba990e0800b86d52cb2aa8511d83a
martin-stierlen <martin.stierlen@tum.de>
2026-02-07 12:32:45 +0100

    chore(frontend): remove unused files

--- frontend/src/types/enums.ts
+++ frontend/src/types/enums.ts
@@ -3,20 +3,3 @@ export enum BlockType {
   PRACTICE = "Practice",
   SUMMARY = "Summary",
 }
-
-export enum BloomsLevel {
-  REMEMBER = "Remember",
-  UNDERSTAND = "Understand",
-  APPLY = "Apply",
-  ANALYZE = "Analyze",
-  EVALUATE = "Evaluate",
-  CREATE = "Create",
-}
-
-export enum SoloLevel {
-  PRESTRUCTURAL = "Prestructural",
-  UNISTRUCTURAL = "Unistructural",
-  MULTISTRUCTURAL = "Multistructural",
-  RELATIONAL = "Relational",
-  EXTENDED_ABSTRACT = "ExtendedAbstract",
-}
