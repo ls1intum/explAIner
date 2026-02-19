938dff8cc95e58f4b098ddcd0da542db0d51ddac
martin-stierlen <martin.stierlen@tum.de>
2026-02-19 11:45:46 +0100

    refactor(backend): remove unused export of baseBlockSchema from block schema

--- backend/src/domain/schemas/blocks/block.schema.ts
+++ backend/src/domain/schemas/blocks/block.schema.ts
@@ -16,4 +16,3 @@ export const blockSchema = z.discriminatedUnion('type', [
   summaryBlockSchema,
 ]);
 export type Block = z.infer<typeof blockSchema>;
-export { baseBlockSchema } from './base-block.schema';
